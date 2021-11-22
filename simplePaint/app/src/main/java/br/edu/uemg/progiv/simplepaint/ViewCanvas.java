e465d23bb1bebe9a6772572851c17301a863c381
Eduardo Henrique Marques Ferreira <eduardo_hen@yahoo.com.br>
2021-11-22 20:27:52 -0300

    Adicionando classe Linha

--- simplePaint/app/src/main/java/br/edu/uemg/progiv/simplepaint/ViewCanvas.java
+++ simplePaint/app/src/main/java/br/edu/uemg/progiv/simplepaint/ViewCanvas.java
@@ -1,13 +1,23 @@
 package br.edu.uemg.progiv.simplepaint;
 
+import android.content.Context;
 import android.graphics.Bitmap;
 import android.graphics.Canvas;
 import android.graphics.Path;
+import android.util.AttributeSet;
+import android.view.View;
 
-public class ViewCanvas {
+import androidx.annotation.Nullable;
+
+public class ViewCanvas extends View {
     private Path path;
     private Bitmap bitmap;
     private Canvas canvas;
     private float fixoX, fixoY;
     private int TOLERANCIA_MOVIMENTO = 5;
+
+    public ViewCanvas(Context context, @Nullable AttributeSet attrs){
+        super(context, attrs);
+    }
+
 }
