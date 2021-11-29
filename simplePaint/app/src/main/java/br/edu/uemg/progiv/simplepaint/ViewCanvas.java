3eb0c11bc8bc0c74bfebaa426a492ef933e661df
Eduardo Henrique Marques Ferreira <eduardo_hen@yahoo.com.br>
2021-11-29 19:39:37 -0300

    Código parcial do app simplePaint, dia 29/11/2021

--- simplePaint/app/src/main/java/br/edu/uemg/progiv/simplepaint/ViewCanvas.java
+++ simplePaint/app/src/main/java/br/edu/uemg/progiv/simplepaint/ViewCanvas.java
@@ -5,6 +5,7 @@ import android.graphics.Bitmap;
 import android.graphics.Canvas;
 import android.graphics.Path;
 import android.util.AttributeSet;
+import android.view.MotionEvent;
 import android.view.View;
 
 import androidx.annotation.Nullable;
@@ -17,13 +18,21 @@ public class ViewCanvas extends View {
     private int TOLERANCIA_MOVIMENTO = 5;
     private Linha linha;
 
+    //método construtor
     public ViewCanvas(Context context, @Nullable AttributeSet attrs){
         super(context, attrs);
+        inicializaObjetos();
     }
 
     private void inicializaObjetos(){
         path = new Path();
         linha = new Linha(getContext(), path);
+    }
+
+    private void inicioToque(float x, float y){
+        path.moveTo(x, y);
+        eixoX = x;
+        eixoY = y;
     }
 
     private void moveuDedoTela(float x, float y){
@@ -43,9 +52,42 @@ public class ViewCanvas extends View {
         path.lineTo(eixoX, eixoY);
     }
 
-    private void limparCanvas(){
+    public void limparCanvas(){
         path.reset();
         invalidate();
     }
 
+    @Override
+    protected void onDraw(Canvas canvas) {
+        super.onDraw(canvas);
+        linha.desenharLinha(canvas);
+    }
+
+    @Override
+    protected void onSizeChanged(int w, int h, int oldw, int oldh) {
+        super.onSizeChanged(w, h, oldw, oldh);
+        bitmap = Bitmap.createBitmap(w, h, Bitmap.Config.ARGB_8888);
+        canvas = new Canvas(bitmap);
+    }
+
+    @Override
+    public boolean onTouchEvent(MotionEvent event) {
+        float x = event.getX();
+        float y = event.getY();
+        switch (event.getAction()){
+            case MotionEvent.ACTION_DOWN:
+                inicioToque(x,y);
+                invalidate();
+                break;
+            case MotionEvent.ACTION_MOVE:
+                moveuDedoTela(x, y);
+                invalidate();
+                break;
+            case MotionEvent.ACTION_UP:
+                tirouDedoTela();
+                invalidate();
+                break;
+        }
+        return true;
+    }
 }
