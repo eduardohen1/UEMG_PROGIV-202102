13288376774f7b88a3384cfd8fc0a67a9a4a5ae8
Eduardo Henrique Marques Ferreira <eduardo_hen@yahoo.com.br>
2022-02-21 20:16:15 -0300

    AppUseCameraKotlin parcial 1

--- AppUseCameraKotlin/app/src/main/java/br/edu/uemg/progiv/appusecamerakotlin/MainActivity.kt
+++ AppUseCameraKotlin/app/src/main/java/br/edu/uemg/progiv/appusecamerakotlin/MainActivity.kt
@@ -1,11 +1,115 @@
 package br.edu.uemg.progiv.appusecamerakotlin
 
+import android.content.Context
+import android.content.pm.PackageManager
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
+import android.widget.Toast
+import androidx.camera.core.CameraSelector
+import androidx.camera.core.ImageCapture
+import androidx.camera.core.Preview
+import androidx.camera.lifecycle.ProcessCameraProvider
+import androidx.core.app.ActivityCompat
+import androidx.core.content.ContextCompat
+import br.edu.uemg.progiv.appusecamerakotlin.databinding.ActivityMainBinding
+import java.io.File
+import java.lang.Exception
+import java.util.concurrent.ExecutorService
+import java.util.concurrent.Executors
 
 class MainActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivityMainBinding
+    private var imageCapture: ImageCapture? = null
+    private lateinit var outputDirectory: File
+    private lateinit var cameraExcutor: ExecutorService
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+        outputDirectory = getOutputDirectory()
+        cameraExcutor = Executors.newSingleThreadExecutor()
+        //permissão
+        if(allPermissionGranted()){
+            //start aqui
+        }else{
+            ActivityCompat.requestPermissions(
+                this,
+                Constants.REQUIRED_PERMISSION,
+                Constants.REQUEST_CODE_PERMISSIONS
+            )
+        }
     }
+
+    private fun getOutputDirectory() : File {
+        val mediaDir = externalMediaDirs.firstOrNull()?.let {
+            mFile -> File(
+                        mFile,
+                        resources.getString(R.string.app_name)
+            ).apply {
+                mkdirs()
+            }
+        }
+        return if(mediaDir != null && mediaDir.exists())
+            mediaDir
+        else
+            filesDir
+    }
+
+    private fun allPermissionGranted() =
+        Constants.REQUIRED_PERMISSION.all {
+            ContextCompat.checkSelfPermission(
+                baseContext, it
+            ) == PackageManager.PERMISSION_GRANTED
+        }
+
+    override fun onRequestPermissionsResult(
+        requestCode: Int,
+        permissions: Array<out String>,
+        grantResults: IntArray
+    ) {
+        super.onRequestPermissionsResult(requestCode, permissions, grantResults)
+        if(requestCode == Constants.REQUEST_CODE_PERMISSIONS){
+            if(allPermissionGranted()){
+                //start aqui
+            }else{
+                Toast.makeText(
+                    this,
+                    "Permissões não concedidas pelo usuário",
+                    Toast.LENGTH_SHORT
+                ).show()
+                finish()
+            }
+        }
+    }
+
+    private fun startCamera(){
+        val cameraProviderFuture = ProcessCameraProvider.getInstance(this)
+        cameraProviderFuture.addListener({
+            val cameraProvider: ProcessCameraProvider = cameraProviderFuture.get()
+            val preview = Preview.Builder()
+                .build()
+                .also {
+                    mPreview -> mPreview.setSurfaceProvider(
+                        binding.cameraPreview.surfaceProvider
+                    )
+                }
+            imageCapture = ImageCapture.Builder().build()
+            val cameraSelector = CameraSelector.DEFAULT_BACK_CAMERA
+            try{
+                cameraProvider.unbindAll()
+                cameraProvider.bindToLifecycle(
+                    this,
+                    cameraSelector,
+                    preview,
+                    imageCapture
+                )
+            }catch (e: Exception){
+                Log.d(Constants.TAG, "startCamera Fail: ", e)
+            }
+        }, ContextCompat.getMainExecutor(this))
+    }
+
 }
